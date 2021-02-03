9dda8a7f5a9d1d968813a204818b8a9d4ced3917
Jin <rifampicin@naver.com>
2021-02-03 04:46:14 +0000

    name blank true

--- first/models.py
+++ first/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class InfectedPlace(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, null=True, blank=True)
     address = models.CharField(max_length=200, null=True, blank=True)
     lat = models.DecimalField(max_digits=20, decimal_places=16)
     lng = models.DecimalField(max_digits=20, decimal_places=16)
