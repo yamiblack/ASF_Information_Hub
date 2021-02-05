a152db8d54e8403c3dd4e36cf80088369ac95af4
Jin <rifampicin@naver.com>
2021-02-05 11:49:00 +0000

    setting

--- asfdjango/settings.py
+++ asfdjango/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,8 +123,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 STATICFILES_DIRS = (
-    BASE_DIR / "static",
+    BASE_DIR / "first/static",
 )
 
 CORS_ORIGIN_ALLOW_ALL = True
