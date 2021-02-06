b604a0d1d0f71dfd9cf340d461b4b32a84ce4d32
Jin <rifampicin@naver.com>
2021-02-06 05:33:58 +0000

    date

--- first/models.py
+++ first/models.py
@@ -7,7 +7,7 @@ class InfectedPlace(models.Model):
     address = models.CharField(max_length=200, null=True, blank=True)
     lat = models.DecimalField(max_digits=20, decimal_places=16)
     lng = models.DecimalField(max_digits=20, decimal_places=16)
-    happened_at = models.DateTimeField(null=True, blank=True)
+    happened_at = models.CharField(max_length=30, null=True, blank=True)
     reported2oie_at = models.DateTimeField(null=True, blank=True)
     memo = models.TextField(null=True, blank=True)
     password = models.CharField(max_length=10, null=True, blank=True)
